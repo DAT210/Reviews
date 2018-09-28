56f50cfbad3079a96bc5506b4a21f1c7c0c55d38
Exerian <oav@live.no>
2018-09-28 13:08:51 +0200

    Added folders

--- flaskr/review.py
+++ flaskr/review.py
@@ -1,7 +1,7 @@
 
 import db
 
-def set(id):
+def set(id, review):
     database = db.get_db()
     cursor = database.cursor()
     try:
@@ -9,4 +9,15 @@ def set(id):
     return
 
 def get(id):
+    database = db.get_db()
+    cursor = database.cursor()
+    try:
+        sql = "SELECT review FROM reviews WHERE id=%s"
+        cursor.execute(sql, (id,))
+        review = cursor.fetchone()
+        return review
+    except pk as err:
+        flash(err, category='error')
+    finally:
+        cursor.close()
     return
