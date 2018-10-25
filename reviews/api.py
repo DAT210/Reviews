317f8596b5a3d5451562d5b6db8499dd38232ac2
Exerian <oav@live.no>
2018-10-25 10:08:30 +0200

    Added support for adding comments to a review.

--- reviews/api.py
+++ reviews/api.py
@@ -3,7 +3,9 @@
 from flask import (
 	Flask, g, jsonify, make_response, request, Blueprint
 )
-from reviews import review
+from reviews.review import (
+	Rating, Comment
+)
 from reviews.exceptions import APIError
 
 bp = Blueprint('api',__name__,url_prefix='/api/1.0')
@@ -25,11 +27,22 @@ def test():
 
 @bp.route('/reviews/<string:meal_id>/', methods=['GET'])
 def get_review(meal_id):
-	rating = review.get(meal_id)
+	rating = Rating.get(meal_id)
 	if rating is None:
 		raise APIError(f"The rating for id '{meal_id}' could not be found", status_code=404, type='Not Found')
 	elif rating is isinstance(rating, Exception):
 		raise APIError(str(rating), status_code=400)
+
+	comments = Comment.get(meal_id)
+	if comments is None or isinstance(comments, Exception):
+		comments_reply = None
+	else:
+		comments_reply = []
+		for (rating, comment,) in comments:
+			comments_reply.append({
+				'rating': rating,
+				'comment': comment
+			})
 
 	reply = {
 		'status': 'success',
@@ -37,17 +50,18 @@ def get_review(meal_id):
 			'review': {
 				'id': meal_id,
 				'rating': rating,
-				'description': f"The id {meal_id} has a rating of {rating} stars."
+				'description': f"The id {meal_id} has a rating of {rating} stars.",
+				'comments': comments_reply
 			}
 		}
-	}
+	}	
 
 	return make_response(jsonify(reply)), 200
 
 
 @bp.route('/reviews/', methods=['GET'])
 def get_reviews():
-	reviews = review.pull()
+	reviews = Rating.pull()
 	if reviews is None:
 		raise APIError("There are no reviews.", status_code=404, type='Not Found')
 
@@ -58,10 +72,22 @@ def get_reviews():
 
 	replies = reply['data']['reviews']
 	for meal_id, rating in reviews:
+		comments = Comment.get(meal_id)
+		if comments is None or isinstance(comments, Exception):
+			comments_reply = None
+		else:
+			comments_reply = []
+			for (rating, comment,) in comments:
+				comments_reply.append({
+				'rating': rating,
+				'comment': comment
+			})
+
 		replies.append({
 			'id': meal_id,
 			'rating': rating,
-			'description': f"The id {meal_id} has a rating of {rating} stars."
+			'description': f"The id {meal_id} has a rating of {rating} stars.",
+			'comments': comments_reply
 		})
 
 	return make_response(jsonify(reply)), 200
@@ -80,6 +106,7 @@ def set_review():
 	try:
 		meal_id = data.pop('id')
 		rating = data.pop('rating')
+		comment = data.pop('comment')
 	except KeyError as err:
 		raise APIError(f"The key {err} could not be found, check documentation for correct request format."
 			, status_code=400, type='Bad Request')
@@ -91,10 +118,13 @@ def set_review():
 		raise APIError(f"The rating can't be greater than 5, or less than 1, was {rating}.", 
 			status_code=400, type='Bad Request')
 
-	check = review.set(meal_id, rating)
+	check = Rating.set(meal_id, rating)
 	if check is None:
 		raise APIError(f"The ID '{meal_id}' does not exist.", status_code=404, type='Not Found')
 
+	check = Comment.set(meal_id, rating, comment) # TODO: Must fix, rating can't be set if comment can't be set.
+	if isinstance(check, Exception):
+		raise APIError(check, status_code=404, type='Not Found')
 	reply = {
 		'status': 'success',
 		'data': None
@@ -112,7 +142,7 @@ def add_review():
 		raise APIError("The json request is missing 'data', " +
 			"read the documentation for the correct format.", status_code=400, type='Bad Request')
 
-	check = review.add(request.get_json()['data'])
+	check = Rating.add(request.get_json()['data'])
 	if isinstance(check, Exception):
 		raise APIError(str(check), status_code=409, type='Conflict')
 
@@ -126,7 +156,7 @@ def add_review():
 
 @bp.route('/reviews/<string:meal_id>/', methods=['DELETE'])
 def remove_review(meal_id):
-	check = review.remove(meal_id)
+	check = Rating.remove(meal_id)
 	if isinstance(check, Exception):
 		raise APIError(str(check), status_code=404, type='Not Found')
 	elif check is 0:
--- reviews/review.py
+++ reviews/review.py
@@ -1,95 +1,134 @@
 '''The Review portion of the Reviews API'''
+import random
 from reviews.db import (
 	get_db, mysql
 )
 
-def set(id, rating):
-	db = get_db()
-	cursor = db.cursor()
-	try:
-		sql = "SELECT score, nr_of_ratings FROM reviews_db.review_meals WHERE meal_id=%s"
-		cursor.execute(sql, (id,))
-		fetch = cursor.fetchone()
-		if fetch is None:
-			return
-		(score, nr_of_ratings) = fetch
-		score += rating
-		nr_of_ratings += 1
-		rating = round((score/nr_of_ratings), 1)
-		sql = "UPDATE reviews_db.review_meals SET rating=%s, score=%s, nr_of_ratings=%s WHERE meal_id=%s"
-		cursor.execute(sql, (rating, score, nr_of_ratings, id,))
-		set_check = cursor.rowcount
-		db.commit()
-		return set_check
-	except mysql.connector.Error as err:
-		print(str(err))
-	finally:
-		cursor.close()
-	return
+class Rating():
+	"""Used for setting, adding, and getting ratings of an object."""
+
+	def set(id, rating):
+		db = get_db()
+		cursor = db.cursor()
+		try:
+			sql = "SELECT score, nr_of_ratings FROM reviews_db.review_meals WHERE meal_id=%s"
+			cursor.execute(sql, (id,))
+			fetch = cursor.fetchone()
+			if fetch is None:
+				return
+			(score, nr_of_ratings) = fetch
+			score += rating
+			nr_of_ratings += 1
+			rating = round((score/nr_of_ratings), 1)
+			sql = "UPDATE reviews_db.review_meals SET rating=%s, score=%s, nr_of_ratings=%s WHERE meal_id=%s"
+			cursor.execute(sql, (rating, score, nr_of_ratings, id,))
+			set_check = cursor.rowcount
+			db.commit()
+			return set_check
+		except mysql.connector.Error as err:
+			print(str(err))
+		finally:
+			cursor.close()
+		return
 
 
-def get(id):
-	db = get_db()
-	cursor = db.cursor()
-	try:
-		sql = "SELECT rating FROM reviews_db.review_meals WHERE meal_id=%s"
-		cursor.execute(sql, (id,))
-		rating = cursor.fetchone()
-		if rating is not None:
-			(rating,) = rating
-		return rating
-	except mysql.connector.Error as err:
-		print(f"Error_get({id}): {err}")
-		return err
-	finally:
-		cursor.close()
-	return
+	def get(id):
+		db = get_db()
+		cursor = db.cursor()
+		try:
+			sql = "SELECT rating FROM reviews_db.review_meals WHERE meal_id=%s"
+			cursor.execute(sql, (id,))
+			rating = cursor.fetchone()
+			if rating is not None:
+				(rating,) = rating
+			return rating
+		except mysql.connector.Error as err:
+			print(f"Error_get({id}): {err}")
+			return err
+		finally:
+			cursor.close()
+		return
 
 
-def pull():
-	db = get_db()
-	cursor = db.cursor()
-	try:
-		sql = "SELECT meal_id, rating FROM reviews_db.review_meals"
-		cursor.execute(sql,)
-		return cursor.fetchall()
-	except mysql.connector.Error as err:
-		print(f"Error_get(): {err}")
-	finally:
-		cursor.close()
-	return
+	def pull():
+		db = get_db()
+		cursor = db.cursor()
+		try:
+			sql = "SELECT meal_id, rating FROM reviews_db.review_meals"
+			cursor.execute(sql,)
+			return cursor.fetchall()
+		except mysql.connector.Error as err:
+			print(f"Error_get(): {err}")
+		finally:
+			cursor.close()
+		return
 
 
-def remove(id):
-	db = get_db()
-	cursor = db.cursor()
-	try:
-		sql = "DELETE FROM reviews_db.review_meals WHERE meal_id=%s"
-		cursor.execute(sql, (id,))
-		delete_check = cursor.rowcount
-		db.commit()
-		return delete_check
-	except mysql.connector.Error as err:
-		print(f"Error_remove: {err}")
-		return err
-	finally:
-		cursor.close()
-	return
+	def remove(id):
+		db = get_db()
+		cursor = db.cursor()
+		try:
+			sql = "DELETE FROM reviews_db.review_meals WHERE meal_id=%s"
+			cursor.execute(sql, (id,))
+			delete_check = cursor.rowcount
+			db.commit()
+			return delete_check
+		except mysql.connector.Error as err:
+			print(f"Error_remove: {err}")
+			return err
+		finally:
+			cursor.close()
+		return
 
 
-def add(meal_ids):
-	db = get_db()
-	cursor = db.cursor()
-	insert_check = 0
-	try:
-		sql = "INSERT INTO reviews_db.review_meals (meal_id) VALUES (%s);"
-		for meal_id in meal_ids:
-			cursor.execute(sql, (meal_id,))
-			insert_check += cursor.rowcount
-		db.commit()
-	except mysql.connector.Error as err:
-		print(f"Error_add: {err}")
-		return err
-	finally:
-		cursor.close()
-	return insert_check
+	def add(meal_ids):
+		db = get_db()
+		cursor = db.cursor()
+		insert_check = 0
+		try:
+			sql = "INSERT INTO reviews_db.review_meals (meal_id) VALUES (%s);"
+			for meal_id in meal_ids:
+				cursor.execute(sql, (meal_id,))
+				insert_check += cursor.rowcount
+			db.commit()
+		except mysql.connector.Error as err:
+			print(f"Error_add: {err}")
+			return err
+		finally:
+			cursor.close()
+		return insert_check
+
+
+class Comment():
+	"""Used for setting and getting comments from the database."""
+
+	def set(the_id, rating, comment):
+		"""Sets a comment in the database."""
+		db = get_db()
+		cursor = db.cursor()
+		try:
+			sql = "INSERT INTO reviews_db.review_comments (comment_id, meal_id, rating, comment) VALUES (%s, %s, %s, %s);"
+			some = random.SystemRandom().getrandbits(16)
+			cursor.execute(sql, (some, the_id, rating, comment,))
+			db.commit()
+			return cursor.rowcount
+		except mysql.connector.Error as err:
+			return err
+		finally:
+			cursor.close()
+		return
+
+	
+	def get(the_id, nr_of_comments=10):
+		"""Gets a comment from the database."""
+		db = get_db()
+		cursor = db.cursor()
+		try:
+			sql = "SELECT rating, comment FROM reviews_db.review_comments WHERE meal_id=%s ORDER BY comment_id DESC LIMIT %s"
+			cursor.execute(sql, (the_id, nr_of_comments,))
+			return cursor.fetchall()
+		except mysql.connector.Error as err:
+			return err
+		finally:
+			cursor.close()
+		return
