6ce0c02722301b285100fd95af130a8a9543572d
Exerian <oav@live.no>
2018-11-19 16:33:43 +0100

    Fixed testcases, and made class methods.

--- test/test_api.py
+++ test/test_api.py
@@ -1,23 +1,29 @@
 '''Testing of the RestfulAPI - Flask'''
 
-from .test import Test
+from .api_test_setup import APITest
 import json
 
 
-class TestApi(Test):
+class TestAPI(APITest):
 
 	def test_get_test(self):
 		response = self.client.get("/api/1.0/test/")
-		self.assertEqual((response.get_json(), response.status_code), ({
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.get_json(), {
 			'status': "Success",
 			'data': {
 				'id': 'test_id',
 				'rating': 5,
 				'description': "The id test_id has a rating of 5 stars."
 			}
-		}, 200))
+		})
 
-	def test_add(self):
+	def test_404(self):
+		"""Will a request to wrong url return a not found?"""
+		response = self.client.get("/nonexist/hello/")
+		self.assertEqual(response.status_code, 404)
+
+	def test_add_201(self):
 		"""Will the id be added in the given format?"""
 		request = json.dumps({
 			'data': [
@@ -26,41 +32,23 @@ class TestApi(Test):
 			]
 		})
 		response = self.api_add(request)
-		self.assertEqual(
-			(response.get_json(), response.status_code),
-			({'status': "success", 'data': None}, 201))
+		self.assertEqual(response.status_code, 201)
+		self.assertEqual(response.get_json(), {'status': "success", 'data': None})
 
-	def test_add2(self):
+	def test_add_400(self):
 		"""Will the id be added in the given format?"""
 		request = json.dumps({
 			'data': {'id': "test_01", 'name': "First Test"}
 		})
 		response = self.api_add(request)
-		self.assertEqual(
-			(response.get_json(), response.status_code),
-			({'status': "error"}, 404))
+		self.assertEqual(response.status_code, 400)
+		self.assertEqual(response.get_json()['status'], "error")
 
-	def api_add(self, data, content_type='application/json'):
-		return self.client.post(
-			"/api/1.0/reviews/", data=data, follow_redirects=True,
-			content_type='application/json'
-		)
-
-	def api_get(self, id=None):
-		url = "api/1.0/reviews/"
-		if id is not None:
-			url = f"api/1.0/reviews/{id}"
-		return self.client.get(url, follow_redirects=True)
-
-	def api_set(self, id, data, content_type='application/json'):
-		return self.client.patch(
-			"/api/1.0/reviews/", data=data, content_type=content_type,
-			follow_redirects=True
-		)
-
-	def api_delete(self, id, content_type='application/json'):
-		return self.client.delete(f"/api/1.0/reviews/{id}", follow_redirects=True)
-
-
-if __name__ == '__main__':
-	unittest.main()
+	def test_add_409(self):
+		"""Will the new meals be added in the given format?"""
+		request = json.dumps({
+			'data': [{'di': "test_01", 'name': "First Test"}]
+		})
+		response = self.api_add(request)
+		self.assertEqual(response.status_code, 409)
+		self.assertEqual(response.get_json()['status'], "error")
